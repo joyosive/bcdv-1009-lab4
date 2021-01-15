9564a4a7d19f6732d5cbb684f5cbcd693fe1a4fe
natasha-rupani <natasha@Natasha-iMac.local>
2021-01-14 20:54:29 -0500

    Greeting Message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import './modules/Greeting';
+import Greeting from './modules/Greeting';
 
 function App() {
   return (
@@ -7,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Greeting></Greeting>
         </p>
         <a
           className="App-link"
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+     
     </div>
   );
 }
---
+++ src/modules/Greeting.js
@@ -0,0 +1,9 @@
+const Greeting = () => {
+    return ( 
+        <p>
+            Hello Welcome to Lab4 By Joy and Natasha
+        </p>
+     );
+}
+ 
+export default Greeting;
